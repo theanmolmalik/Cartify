a13e898b0e3625d7ae4ceb1a02cedf92f022ff91
Anmol Malik <68939602+theanmolmalik@users.noreply.github.com>
2022-07-28 15:55:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import App from "./components/App";
 import {BrowserRouter} from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-const domain = "theanmolmalik.us.auth0.com";
-const clientId = "3aOvrADK2RLnkm4vvToS2PFHFgd9QlFw";
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
     <BrowserRouter>
